750cdfc05d2ebace2795aaa13668243c55c1f40f
Jon Struve <ocdammo3@gmail.com>
2019-11-03 08:54:28 -0600

    Changed response component to functional

--- src/components/Response.js
+++ src/components/Response.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Response extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="responseBody">
-          <p>Headers</p>
-          <p>Response</p>
-        </div>
-      </>
-    )
-  }
+function Response(props) {
+  return (
+    <>
+      <div className="responseBody">
+        <p>Headers</p>
+        <p>Response</p>
+      </div>
+    </>
+  )
 }
 
 export default Response;
