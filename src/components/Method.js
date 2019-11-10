0a93c1a7e94702e8e72eaff9f6da44a1598be11b
Jon Struve <ocdammo3@gmail.com>
2019-11-10 09:07:52 -0600

    Changed second file back to class

--- src/components/Method.js
+++ src/components/Method.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-function Method(props) {
-  const handleClick = e => {
+class Method extends React.Component {
+  handleClick = e => {
     e.preventDefault();
   }
   
-  return (
-    <>
-      <div className="httpMethods">
-        <form>
-          <label><input name="methodType" type="radio" className="methodChecks"/>GET</label>
-          <label><input name="methodType"  type="radio" className="methodChecks"/>POST</label>
-          <label><input name="methodType"  type="radio" className="methodChecks"/>PUT</label>
-          <label><input name="methodType"  type="radio" className="methodChecks"/>PATCH</label>
-          <label><input name="methodType"  type="radio" className="methodChecks"/>DELETE</label>
-          <button onClick={handleClick} name="submitMethod">Go!</button>
-        </form>
-      </div>
-    </>
-  )
+  render() {
+    return (
+      <>
+        <div className="httpMethods">
+          <form>
+            <label><input name="methodType" type="radio" className="methodChecks"/>GET</label>
+            <label><input name="methodType"  type="radio" className="methodChecks"/>POST</label>
+            <label><input name="methodType"  type="radio" className="methodChecks"/>PUT</label>
+            <label><input name="methodType"  type="radio" className="methodChecks"/>PATCH</label>
+            <label><input name="methodType"  type="radio" className="methodChecks"/>DELETE</label>
+            <button onClick={this.handleClick} name="submitMethod">Go!</button>
+          </form>
+        </div>
+      </>
+    )
+  }
 }
 
 export default Method;
