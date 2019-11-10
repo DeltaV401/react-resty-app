a14024dbbd5ab62b429b30ae9093c66f102286e7
Jon Struve <ocdammo3@gmail.com>
2019-11-10 10:05:34 -0600

    Changed reducers to add login and removed history

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,12 @@ import { composedWithDevTools } from 'redux-devtools-extension';
 
 import { reducer as requestForm } from './request-form-reducer';
 import { reducer as requestBody } from './request-body-reducer';
-import { reducer as responseBody } from './response-body-reducer;'
-import { reducer as history } from './history-reducer';
+import { reducer as login } from './login-reducer;'
 
 let reducer = combineReducers({
   requestForm,
   requestBody,
-  responseBody,
-  history,
+  login,
 });
 
 export default function() {
